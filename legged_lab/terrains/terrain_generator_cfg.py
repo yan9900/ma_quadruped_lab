c231715fc64180b4db2ce1c705d48471aa96f66b
Hellod035 <hellod035@163.com>
2025-04-26 10:23:22 +0800

    Updated to isaaclab2.1.0 Added complex terrain

--- legged_lab/terrains/terrain_generator_cfg.py
+++ legged_lab/terrains/terrain_generator_cfg.py
@@ -45,43 +45,54 @@ ROUGH_TERRAINS_CFG = TerrainGeneratorCfg(
     use_cache=False,
     sub_terrains={
         "pyramid_stairs_28": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.2,
-            step_height_range=(0.05, 0.23),
+            proportion=0.1,
+            step_height_range=(0.0, 0.23),
             step_width=0.28,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
         "pyramid_stairs_30": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.2,
-            step_height_range=(0.05, 0.23),
+            proportion=0.1,
+            step_height_range=(0.0, 0.23),
             step_width=0.30,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
         "pyramid_stairs_32": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.2,
-            step_height_range=(0.05, 0.23),
+            proportion=0.1,
+            step_height_range=(0.0, 0.23),
             step_width=0.32,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
         "pyramid_stairs_34": terrain_gen.MeshInvertedPyramidStairsTerrainCfg(
-            proportion=0.2,
-            step_height_range=(0.05, 0.23),
+            proportion=0.1,
+            step_height_range=(0.0, 0.23),
             step_width=0.34,
             platform_width=3.0,
             border_width=1.0,
             holes=False,
         ),
         "boxes": terrain_gen.MeshRandomGridTerrainCfg(
-            proportion=0.2, grid_width=0.45, grid_height_range=(0.05, 0.15), platform_width=2.0
+            proportion=0.15, grid_width=0.45, grid_height_range=(0.0, 0.15), platform_width=2.0
         ),
         "random_rough": terrain_gen.HfRandomUniformTerrainCfg(
-            proportion=0.2, noise_range=(0.02, 0.10), noise_step=0.02, border_width=0.25
+            proportion=0.15, noise_range=(-0.02, 0.04), noise_step=0.02, border_width=0.25
         ),
-
+        "wave": terrain_gen.HfWaveTerrainCfg(
+            proportion=0.15, amplitude_range=(0.0, 0.2), num_waves=5.0
+        ),
+        "high_platform": terrain_gen.MeshPitTerrainCfg(
+            proportion=0.15, pit_depth_range=(0.0, 0.3), platform_width=2.0, double_pit=True
+        ),
+        # "star": terrain_gen.MeshStarTerrainCfg(
+        #     proportion=0.15, num_bars=6, bar_width_range=(0.05, 0.05), bar_height_range=(0.0, 0.25), platform_width=1.0
+        # ),
+        # "gap": terrain_gen.MeshGapTerrainCfg(
+        #     proportion=0.15, gap_width_range=(0.1, 0.4), platform_width=2.0
+        # )
     },
 )
